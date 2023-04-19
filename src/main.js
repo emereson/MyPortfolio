516fee4f7ccb4ad25806a6ffd4706a9ab1f725c6
emerson <escanorrosa123@gmail.com>
2023-04-19 16:17:22 -0500

    my portfolio

--- src/main.js
+++ src/main.js
@@ -1,18 +1,14 @@
-const homeNav = document.querySelector('.homeNav')
-const menuIcon = document.querySelector('.bxs-grid-alt')
-const IconClose = document.querySelector('.bxs-x-circle')
-const contenedor_loader = document.querySelector('.loader__container')
+const homeNav = document.querySelector(".homeNav");
+const menuIcon = document.querySelector(".bxs-grid-alt");
+const IconClose = document.querySelector(".bxs-x-circle");
+const contenedor_loader = document.querySelector(".loader__container");
 
-menuIcon.addEventListener('click', function () 
-{
-    menuIcon.classList.toggle('bxs-x-circle')
-    homeNav.classList.toggle('homeNavShow')
-})
-menuIcon.addEventListener('click', function () 
-{
-    IconClose? homeNav.style.transition = '0.5s': homeNav.style.transition ='0.5s'
-})
-setTimeout(function (){
-    contenedor_loader.style.opacity = 0
-    contenedor_loader.style.visibility = 'hidden'
-},3000) 
+menuIcon.addEventListener("click", function () {
+  menuIcon.classList.toggle("bxs-x-circle");
+  homeNav.classList.toggle("homeNavShow");
+});
+menuIcon.addEventListener("click", function () {
+  IconClose
+    ? (homeNav.style.transition = "0.5s")
+    : (homeNav.style.transition = "0.5s");
+});
